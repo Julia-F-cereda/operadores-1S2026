ca85117cdebb95338123e4a9308a43fb7b5d14b8
julia cereda <julia.cereda@aluno.senai.br>
2026-02-12 11:26:28 -0300

    COMITY: fatorando codigo

--- operadores-v3-1.js
+++ operadores-v3-1.js
@@ -100,11 +100,27 @@ function dividir(){
 };
 
 function porcentagem(){
+    const v01 = parseFloat(input01.value);
+    const v02 = parseFloat(input02.value);
+    
+     //confere se tem valor que nao é numero
+     if (isNaN(v01) || isNaN(v02)) {
+        alert('Um dos valores esta invalido');
+     } else{
+        //exibe a area de resultado
+        resultado.style.display = 'flex';
+
+        //exibe a mensagem com o calculo
+    resultado.innerHTML = ` 
+            <h2>Memória de calculo</h2>
+            <ul>
+            <li>operação: <span>SOMA</span> </li>
+            <li>Valor 01 digitado pelo usuário: <span> ${v01} </span> </li>
+            <li>Valor 02 digitado pelo usuário:<span> ${v02} </span> </li>       
+            <li>O resultado da operação é ${v01} / ${v02} = <span> ${v02 / 100}</span></li>
+        </ul>`
+     }
 };
 
 function calcPagVista(){};
 function calcPagVista(){};
-
-function limpar(){
-
-};
